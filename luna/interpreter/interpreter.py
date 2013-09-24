4440cc27a71a427799e293e7a515a6f18da7fd14
Martin Matusiak <numerodix@gmail.com>
2013-09-24 21:19:28 +0200

    simplify eval visitor

--- luna/interpreter/interpreter.py
+++ luna/interpreter/interpreter.py
@@ -1,30 +1,16 @@
-class GenericVisitor(object):
-    def visit(self, node):
-        clsname = node.__class__.__name__.lower()
-        method = getattr(self, 'visit_%s' % clsname, None)
-
-        if method:
-            return method(node)
-        else:
-            self.generic_visit(node)
-
-    def generic_visit(self, node):
-        return vc
+from luna.ast.visitors import GenericVisitor
 
 
 class EvalVisitor(GenericVisitor):
-    def visit_expr(self, node):
-        return self.visit_binop(node.value)
+    def visit_expr(self, node, vc):
+        return vc[0]
 
-    def visit_binop(self, node):
-        left, op, right = list(node)
-        left = self.visit_number(left)
-        op = self.visit_operator(op)
-        right = self.visit_number(right)
+    def visit_binop(self, node, vc):
+        left, op, right = list(vc)
         return eval('%s %s %s' % (left, op, right))
 
-    def visit_operator(self, node):
+    def visit_operator(self, node, vc):
         return node.value
 
-    def visit_number(self, node):
+    def visit_number(self, node, vc):
         return float(node.value)
