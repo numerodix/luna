3a9d46944fcb676d0f3e904dfa1bc112594bdb4b
Martin Matusiak <numerodix@gmail.com>
2013-09-29 18:58:17 +0200

    add ast tests

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-coverage run --source . -m py.test
+coverage run --source . -m py.test $@
 coverage report
--- tests/ast/test_ast.py
+++ tests/ast/test_ast.py
@@ -3,11 +3,23 @@ import pytest
 from luna.ast import *
 
 
-def test_eq():
+def test_eq1():
     assert Boolean('true') == Boolean('true')
 
-def test_neq():
+def test_eq2():
+    assert Nil() == Nil()
+
+def test_eq3():
+    assert Args(Boolean('true'), Number('1')) == Args(Boolean('true'), Number('1'))
+
+def test_neq1():
     assert Boolean('true') != Boolean('false')
+
+def test_neq2():
+    assert Boolean('true') != Nil()
+
+def test_neq3():
+    assert Args(Boolean('true')) != Args(Boolean('true'), Number('1'))
 
 def test_invalid():
     with pytest.raises(ValueError):
